13b877c72a7323e5406bd4fe3e51010572fb0e29
abhisheklokanthan <abhisheklokanthan@gmail.com>
2025-07-18 23:40:55 +0530

    TF workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,34 @@
 resource "aws_s3_bucket" "source" {
     bucket = var.aws_s3_bucket  
 }
+
+resource "aws_s3_bucket_website_configuration" "static_website_config" {
+    bucket = aws_s3_bucket.source.id
+    index_document {
+      suffix = "index.html"
+    }
+}
+
+resource "aws_s3_bucket_policy" "static_site_policy" {
+    bucket = aws_s3_bucket.source.id
+   policy = jsondecode({
+            version = "2012-10-17"
+            statement = [
+                {
+                    Effect = "Allow"
+                    Principal = "*"
+                    Action = "s3:GetObject"
+                    Resource = "${aws_s3_bucket.source.arn}/*"
+                }
+            ]
+   })
+  depends_on = [ aws_s3_account_public_access_block.static_site_access ]
+}
+
+resource "aws_s3_account_public_access_block" "static_site_access" {
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+    
+}
