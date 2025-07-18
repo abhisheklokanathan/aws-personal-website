a189e9a550f8986b010d25d5f01dac2b10849ca3
abhisheklokanthan <abhisheklokanthan@gmail.com>
2025-07-19 00:02:41 +0530

    TF workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,9 +9,17 @@ resource "aws_s3_bucket_website_configuration" "static_website_config" {
     }
 }
 
+resource "aws_s3_account_public_access_block" "static_site_access" {
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+    
+}
+
 resource "aws_s3_bucket_policy" "static_site_policy" {
   bucket = aws_s3_bucket.source.id
-
+  
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -26,12 +34,4 @@ resource "aws_s3_bucket_policy" "static_site_policy" {
     ]
   })
   depends_on = [ aws_s3_account_public_access_block.static_site_access ]
-}
-
-resource "aws_s3_account_public_access_block" "static_site_access" {
-    block_public_acls = false
-    block_public_policy = false
-    ignore_public_acls = false
-    restrict_public_buckets = false
-    
 }
