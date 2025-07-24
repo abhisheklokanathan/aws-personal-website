61dd77a412661f67fc12443dc4944d8e84a1d736
abhisheklokanthan <abhisheklokanthan@gmail.com>
2025-07-24 22:55:48 +0530

    ACM

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "source" {
-    bucket = var.aws_s3_bucket  
+    bucket = var.aws_s3_bucket 
 }
 
 resource "aws_s3_bucket_public_access_block" "static_site_access" {
@@ -9,4 +9,17 @@ resource "aws_s3_bucket_public_access_block" "static_site_access" {
     ignore_public_acls = true
     restrict_public_buckets = true
     
+}
+
+resource "aws_acm_certificate" "studysite_cert" {
+           domain_name = "studysite.shop"
+           validation_method = "DNS"
+           subject_alternative_names = "www.studysite.shop"
+
+          tags = {
+            Name = "My SSL cert"
+          }
+          lifecycle {
+            create_before_destroy = true
+          }
 }
