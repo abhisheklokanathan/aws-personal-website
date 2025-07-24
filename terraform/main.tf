c8bd32bf2631cb7754fa793fe053d4f439b7a555
abhisheklokanthan <abhisheklokanthan@gmail.com>
2025-07-24 23:27:51 +0530

    Route 53

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,33 @@ resource "aws_acm_certificate" "studysite_cert" {
             create_before_destroy = true
           }
 }
+
+data "aws_route53_zone" "domain_zone" {
+  name = "studysite.shop"
+  
+}
+
+resource "aws_route53_record" "studysite_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.aws_acm_certificate.studysite_cert.domain_validation_options : dvo.domain_name
+    =>{
+       name = dvo.resource_record_name
+       record = dvo.resource_record_value
+       type = dvo.resource_record_type
+    }
+
+  }
+
+  name = each.value.name
+  records = each.value.key
+  ttl = 60
+  type = each.value.type
+  zone_id = data.aws_route53_zone.zone_id
+
+}
+
+resource "aws_acm_certificate_validation" "studysite_validation" {
+  certificate_arn = aws_acm_certificate.studysite_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.aws_acm_certificate.studysite_validation : record.fqdn ]
+  
+}
