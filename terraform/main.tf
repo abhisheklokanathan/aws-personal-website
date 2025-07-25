7686821941dbfbdd1d584dc93166f6d208444471
abhisheklokanthan <abhisheklokanthan@gmail.com>
2025-07-25 22:27:47 +0530

    Cloudfront

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,3 +59,85 @@ resource "aws_acm_certificate_validation" "studysite_validation" {
   validation_record_fqdns = [for record in aws_route53_record.studysite_validation : record.fqdn ]
   
 }
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "oac${aws_s3_bucket.static_site.bucket}"
+  description                       = "OAC for ${aws_s3_bucket.static_site.bucket}"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.source.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+    origin_id                = "S3-${aws_s3_bucket.static_site.bucket}"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Cloudfront distribution for -${aws_s3_bucket.static_site.bucket}"
+  default_root_object = "index.html"
+
+  aliases = ["studysite.shop", "www.studysite.shop"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.static_site.bucket}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.studysite_cert.arn
+    ssl_support_method = "sni-only"
+  }
+  depends_on = [ aws_acm_certificate_validation.studysite_validation ]
+}
+
+resource "aws_s3_bucket_policy" "static_site_policy" {
+  bucket = aws_s3_bucket.source.id
+  policy = jsonencode({
+    Version = "2022-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action = "s3.GetObject"
+        Resource = "${aws_s3_bucket.static_site.arn}/*"
+        Condition = {
+          StingEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
+          }
+        }
+      }
+    ]
+})
+  
+}
